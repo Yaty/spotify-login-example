4e9d7aae3ce726a16acbd1abb8d29748998d4f64
Da Roit Hugo <contact@hdaroit.fr>
2018-05-17 23:57:36 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const SpotifyWebAPI = require('spotify-web-api-node');
+const express = require('express');
+const app = express();
+
+const spotify = new SpotifyWebAPI({
+    clientId: '',
+    clientSecret: ''
+});
+
+app.use('/', function (req, res, next) {
+    // in this case we are using the Client Credential Flow
+    spotify.clientCredentialsGrant().then(
+        function(data) {
+            res.json(data.body['access_token']);
+        },
+        function(err) {
+            console.log('Error', err);
+            res.sendStatus(500);
+        }
+    );
+});
+
+app.listen(3000, function () {
+    console.log('Listening.');
+});
